633a8dcb1b40c86e28f0fa99b84773d73626db3e
Zhukov Nikita <nypno@mail.ru>
2022-03-02 19:01:12 +0300

    чат с личными сообщениями

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -1,7 +1,22 @@
 package client;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        primaryStage.setTitle("Chatty");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
